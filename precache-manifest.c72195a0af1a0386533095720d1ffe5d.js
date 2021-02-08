380e02fa435fe226ebe4be565271e3395b7338fe
NOrig <sorktt@gmail.com>
2021-02-08 12:32:22 +0600

    Updates

---
+++ precache-manifest.c72195a0af1a0386533095720d1ffe5d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34534d4b47e2d318198a53b42aac5cfc",
+    "url": "/to-do-list.github.io/index.html"
+  },
+  {
+    "revision": "f23839a8db968062bd72",
+    "url": "/to-do-list.github.io/static/css/main.baad310d.chunk.css"
+  },
+  {
+    "revision": "724a384ba518726a60da",
+    "url": "/to-do-list.github.io/static/js/2.c32c6ded.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/to-do-list.github.io/static/js/2.c32c6ded.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f23839a8db968062bd72",
+    "url": "/to-do-list.github.io/static/js/main.5ff30552.chunk.js"
+  },
+  {
+    "revision": "6781c792b9f86bdba859",
+    "url": "/to-do-list.github.io/static/js/runtime-main.528a8077.js"
+  },
+  {
+    "revision": "936f88857e13715003232463b5430273",
+    "url": "/to-do-list.github.io/static/media/edit_icon.936f8885.png"
+  },
+  {
+    "revision": "67166eb15b587a61360b0b6a0828bf7a",
+    "url": "/to-do-list.github.io/static/media/nav_before.67166eb1.png"
+  },
+  {
+    "revision": "1fa8273811e55a27d9842ccfd569043a",
+    "url": "/to-do-list.github.io/static/media/nav_next.1fa82738.png"
+  },
+  {
+    "revision": "bf13633ae6beddfc4662394b1ef91657",
+    "url": "/to-do-list.github.io/static/media/save_icon.bf13633a.png"
+  }
+]);
